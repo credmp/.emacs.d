e77be25ae539974f65614b0547f7fb49e42ad881
Arjen Wiersma <arjen@wiersma.org>
2013-01-05 13:20:59 +0100

    Add hexcolour and activate it
    Also enabled the menu bar on Mac OS X.

--- init.el
+++ init.el
@@ -6,6 +6,7 @@
 
 ;; Turn off mouse interface early in startup to avoid momentary display
 (if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
+(when is-mac (menu-bar-mode -1))
 (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
 (if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
 
@@ -100,6 +101,7 @@
 (require 'setup-paredit)
 (require 'setup-autocomplete)
 (require 'setup-eclim)
+(require 'setup-hexcolour)
 
 ;; Language specific setup files
 (eval-after-load 'js2-mode '(require 'setup-js2-mode))
